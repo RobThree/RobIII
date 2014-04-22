7816dffdac40ba39663ece607a1e4294e84dd755
RobThree <rob@devcorner.nl>
2014-04-22 21:43:49 +0200

    * Split modernizr/respond.js into separate files (are bundled anyway) * Added handlebars

--- RobIII/App_Start/BundleConfig.cs
+++ RobIII/App_Start/BundleConfig.cs
@@ -6,16 +6,23 @@ namespace MvcBootstrap
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //Bootstrap.js / jQuery and modernizr/respond.js are loaded from CDN or separately
+            //Bootstrap.js / jQuery from CDN or separately
+            //Modernizr/respond.js are loaded 
             //TODO: Take a look at http://www.asp.net/mvc/tutorials/mvc-4/bundling-and-minification; turns out CDN's can be used/specified here too! Coolio!
 
-            bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                        "~/js/main.js"
+            bundles.Add(new ScriptBundle("~/bundles/js-head").Include(
+                "~/js/vendor/modernizr-2.7.2.js",
+                "~/js/vendor/respond-1.4.2.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/js-body").Include(
+                "~/js/vendor/handlebars.runtime-v1.3.0.js",
+                "~/js/main.js"
             ));
 
             bundles.Add(new StyleBundle("~/bundles/css").Include(
-                        "~/css/bootstrap.min.css",
-                        "~/css/main.css"
+                "~/css/bootstrap.min.css",
+                "~/css/main.css"
             ));
 
             BundleTable.EnableOptimizations = true;
