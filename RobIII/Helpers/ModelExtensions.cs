2ad50398c4638bf9a28cf3d3cc3fc1f1e1562cd7
RobThree <rob@devcorner.nl>
2014-04-24 02:05:12 +0200

    * Improved language selection

---
+++ RobIII/Helpers/ModelExtensions.cs
@@ -0,0 +1,22 @@
+﻿using RobIII.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RobIII.Helpers
+{
+    public static class ModelExtensions
+    {
+        public static string IsCurrentLanguage(this BlogrollViewmodel model, FeedLanguage language)
+        {
+            return IsCurrentLanguage(model, language.ToString());
+        }
+
+        public static string IsCurrentLanguage(this BlogrollViewmodel model, string language)
+        {
+            return model.Language.Equals(language, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+
+    }
+}
