9e2cd9eee79295fa3bec23a9f6da8f5ffa852f90
RobThree <rob@devcorner.nl>
2014-05-11 22:04:29 +0200

    * Added missing file

---
+++ RobIII/Models/ContactformViewmodel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RobIII.Models
+{
+    public class ContactformViewmodel
+    {
+        [Required]
+        [StringLength(150)]
+        [Display(Order = 1)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Display(Order = 2)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Order = 3)]
+
+        public string Message { get; set; }
+
+        public string Schd { get; set; }
+    }
+}
