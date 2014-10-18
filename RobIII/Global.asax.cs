1c8a7b448d35be5b6e875eb752a7d66401a25fbd
RobThree <rob@devcorner.nl>
2014-10-18 18:29:51 +0200

    * Another try to fix datetime localization...

--- RobIII/Global.asax.cs
+++ RobIII/Global.asax.cs
@@ -10,14 +10,6 @@ namespace RobIII
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_AcquireRequestState(object sender, EventArgs e)
-        {
-            var ci = new CultureInfo("en-US");
-
-            System.Threading.Thread.CurrentThread.CurrentCulture = ci;
-            System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
-        }
-
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
