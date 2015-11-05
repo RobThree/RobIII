4050be30d6062057c1b703a683623d5fbf713445
RobThree <rob@devcorner.nl>
2015-11-05 18:26:32 +0100

    * Decent reply from mailclient fixed

--- RobIII/Controllers/APIController.cs
+++ RobIII/Controllers/APIController.cs
@@ -93,7 +93,8 @@ namespace RobIII.Controllers
                     smtpServer.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["smtpuser"], ConfigurationManager.AppSettings["smtppass"]);
                 }
 
-                mail.From = new MailAddress(model.Email, model.Name);
+                mail.ReplyToList.Add(new MailAddress(model.Email, model.Name));
+                mail.From = new MailAddress(ConfigurationManager.AppSettings["contactform-recipient"], ConfigurationManager.AppSettings["contactform-recipient-name"]);
                 mail.To.Add(ConfigurationManager.AppSettings["contactform-recipient"]);
                 mail.Subject = "Contact-form";
                 mail.Body = string.Format("{0}\n----\nIP: {1}\nUA: {2}", model.Message, HttpContext.Current.Request.UserHostAddress, HttpContext.Current.Request.UserAgent);
