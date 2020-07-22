455b0092abdba3992d2fdfe83c608a16aab95e6e
RobThree <rob@devcorner.nl>
2020-07-22 21:42:29 +0200

    Start work on V2

--- RobIII/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RobIII.MvcApplication" Language="C#" %>
---
+++ RobIII/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RobIII.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
